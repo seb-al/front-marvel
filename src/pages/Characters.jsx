cca0533445df065bd49fed1d419e061f1067664b
unknown <alexandru.sebs@gmail.com>
2023-05-12 16:31:55 +0200

    adding search input characters

--- src/pages/Characters.jsx
+++ src/pages/Characters.jsx
@@ -3,6 +3,8 @@ import axios from "axios";
 import { Link } from "react-router-dom";
 
 const Characters = () => {
+  const [name, setName] = useState("");
+
   const [data, setData] = useState();
   const [isLoading, setIsLoading] = useState(true);
 
@@ -10,7 +12,7 @@ const Characters = () => {
     const fetchData = async () => {
       try {
         const response = await axios.get(
-          "https://site--marvel--x89fgb8wnx9j.code.run/characters"
+          `https://site--marvel--x89fgb8wnx9j.code.run/characters?name=${name}`
         );
         setData(response.data);
         setIsLoading(false);
@@ -19,42 +21,55 @@ const Characters = () => {
       }
     };
     fetchData();
-  }, []);
+  }, [name]);
 
   return isLoading ? (
     <div className="spinner container-form"></div>
   ) : (
-    <div className="container page-characters">
-      {data.results.map((character) => {
-        if (
-          character.thumbnail.path !==
-          "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available"
-        ) {
-          return (
-            <Link key={character._id} to={`/character/${character._id}`}>
-              <div className="card-characters">
-                <div className="for-cut-characters">
-                  <img
-                    src={
-                      character.thumbnail.path +
-                      "." +
-                      character.thumbnail.extension
-                    }
-                    alt={character.name}
-                  />
+    <>
+      <div className="container div-search">
+        <input
+          className="search-input"
+          type="search"
+          placeholder="Recherche de personnage"
+          value={name}
+          onChange={(event) => {
+            setName(event.target.value);
+          }}
+        />
+      </div>
+      <div className="container page-characters">
+        {data.results.map((character) => {
+          if (
+            character.thumbnail.path !==
+            "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available"
+          ) {
+            return (
+              <Link key={character._id} to={`/character/${character._id}`}>
+                <div className="card-characters">
+                  <div className="for-cut-characters">
+                    <img
+                      src={
+                        character.thumbnail.path +
+                        "." +
+                        character.thumbnail.extension
+                      }
+                      alt={character.name}
+                    />
+                  </div>
+                  <div className="characters-title-desc">
+                    <h3>{character.name}</h3>
+                    <p>{character.description}</p>
+                  </div>
                 </div>
-                <div className="characters-title-desc">
-                  <h3>{character.name}</h3>
-                  <p>{character.description}</p>
-                </div>
-              </div>
-            </Link>
-          );
-        } else {
-          return null;
-        }
-      })}
-    </div>
+              </Link>
+            );
+          } else {
+            return null;
+          }
+        })}
+      </div>{" "}
+    </>
   );
 };
 
