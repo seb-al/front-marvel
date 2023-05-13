bb54fdec5353e8f8e3379557f1b673e010ceee7b
unknown <alexandru.sebs@gmail.com>
2023-05-13 09:57:30 +0200

    before adding localstorage

--- src/pages/Comics.jsx
+++ src/pages/Comics.jsx
@@ -1,7 +1,9 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import { Link } from "react-router-dom";
+import { Link, Navigate } from "react-router-dom";
 import Pages from "../components/Pages";
+import { FaRegStar } from "react-icons/fa";
+import Cookies from "js-cookie";
 
 const Comics = () => {
   const [title, setTitle] = useState("");
